ec3f16a2f4bd01a7fbc687221cce22b5cf040ffd
Thomas Böhm <thomas.boehm@hensoldt-cyber.com>
2020-07-06 19:22:06 +0200

    [DEV] Renamed types from network stack

--- src/TlsServer.c
+++ src/TlsServer.c
@@ -195,7 +195,7 @@ TlsLibServer_getClient()
 static void
 init_network_client_api()
 {
-    static os_network_dataports_socket_t config;
+    static OS_NetworkStackClient_SocketDataports_t config;
     static OS_Dataport_t dataport = OS_DATAPORT_ASSIGN(NwAppDataPort);
 
     config.number_of_sockets = 1;
