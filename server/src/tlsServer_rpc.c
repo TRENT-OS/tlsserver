b34a4d459687316a6438e75ef3e4e8d5bb1057dc
Slawomir Kwasniak <slawomir.kwasniak@hensoldt-cyber.de>
2021-04-14 20:29:42 +0200

    [REF] Unify dataports' names
    Our guidelines are enforcing 'port' suffix when naming dataports.
    
    See:
    SEOS-1626

--- server/src/tlsServer_rpc.c
+++ server/src/tlsServer_rpc.c
@@ -149,7 +149,7 @@ static void
 init_network_client_api()
 {
     static OS_NetworkStackClient_SocketDataports_t config;
-    static OS_Dataport_t dataport = OS_DATAPORT_ASSIGN(network_stack_port);
+    static OS_Dataport_t dataport = OS_DATAPORT_ASSIGN(networkStack_port);
 
     config.number_of_sockets = 1;
 
